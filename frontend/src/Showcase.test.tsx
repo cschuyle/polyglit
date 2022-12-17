4f59b128a9f6326e3e7a7c29c588928a328272e4
Carlton Schuyler <cschuyler@vmware.com>
2022-12-17 13:43:53 -0700

    npm audit --force

--- frontend/src/Showcase.test.tsx
+++ frontend/src/Showcase.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import Showcase from './Showcase';
 
 test('renders the title', () => {
-  render(<Showcase />);
+  render(<Showcase pageHeader='' pageSubtitle='' troveUrl='' collectionTitle='' showDupsCheckbox />);
   const linkElement = screen.getByText(/Little Prince/);
   expect(linkElement).toBeInTheDocument();
 });
